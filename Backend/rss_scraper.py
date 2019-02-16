a4473f42b35b4d1a7f2f1de9b21ab322f7e0b5cc
Rishier827 <nishara.2017431@iit.ac.lk>
2019-02-17 00:41:11 +0530

    Try checking other rss feeds

--- Backend/rss_scraper.py
+++ Backend/rss_scraper.py
@@ -2,5 +2,9 @@ import feedparser
 import json
 
 url = "http://www.adaderana.lk/rss.php"
+# url = "http://www.hirunews.lk/rss/english.xml"
+# url = "http://fe1.virakesari.lk/feed" #getting another language RSS feed
+
+
 feedF = feedparser.parse( url )
 print(feedF['entries'][0]['title'])
