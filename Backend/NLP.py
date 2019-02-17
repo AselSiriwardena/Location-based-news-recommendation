2b9ce81d540488245673d54c5459e8976ba7652f
Rishier827 <nishara.2017431@iit.ac.lk>
2019-02-17 21:42:16 +0530

    replacing with url

--- Backend/NLP.py
+++ Backend/NLP.py
@@ -11,11 +11,13 @@ nltk.download('maxent_ne_chunker')
 nltk.download('words')
 
 
-text = """Kadawatha Opposition Leader Mahinda Rajapaksa says that the whole  public administration has collapsed due to the constitution council’s arbitrary actions.
-The Opposition Leader said so in response to a query a journalised raised  after a meeting held in Malabe and Meegamuwa"""
-places = geograpy.get_place_context(text=text)
-print(places.places)
-
+# text = """Kadawatha Opposition Leader Mahinda Rajapaksa says that the whole  public administration has collapsed due to the constitution council’s arbitrary actions.
+# The Opposition Leader said so in response to a query a journalised raised  after a meeting held in Malabe and Meegamuwa"""
+# places = geograpy.get_place_context(text=text)
+# print(places.places)
+url = 'http://www.bbc.com/news/world-europe-26919928'
+places = geograpy.get_place_context(url=url)
+print(places.cities)
 
 
 print("****************************************************")
