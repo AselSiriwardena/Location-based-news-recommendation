09794d86b983c3f246fe7ece58ad2aea8f9ec749
PeshalaLiyanage <peshala1996@gmail.com>
2019-02-18 21:15:25 +0530

    Merge branch 'master' of /home/peshala/PycharmProjects/Test/Location-based-news-reccomendation with conflicts.

--- Backend/rss_scraper.py
+++ Backend/rss_scraper.py
@@ -2,45 +2,31 @@ import feedparser
 import json
 import geograpy
 
-urlList = ["http://www.adaderana.lk/rss.php",
-           "http://www.hirunews.lk/rss/english.xml",
-           "http://www.gossiplankahotnews.com/feeds/posts/default/-/Hotnews",
-           "https://www.news.lk/news?format=feed",
-           #"https://srilankamirror.com/news?format=feed&type=rss",
-           #"http://www.thesundayleader.lk/feed/",
-           #"http://www.dinamina.lk/rss.xml",
-           #"https://www.newsfirst.lk/feed/"
-           ]
+url = "http://www.adaderana.lk/rss.php"
+# url = "http://www.hirunews.lk/rss/english.xml"
+# url = "http://fe1.virakesari.lk/feed" #getting another language RSS feed
 
-listItem = 0
 
-while (listItem < len(urlList)):
-    print("*** "+urlList[listItem]+" ***")
-    feedF = feedparser.parse(urlList[listItem])
-    feedList = []
-    placesInFeed = []
-    i = 0
-    while (i < len(feedF['entries'])):
-        feedList.append(feedF['entries'][i]['title'])
-        print("feed " + str(i) + " : " + feedList[i])
-        #print("description " + str(i) + " : " + feedF.entries[i].description)
-        i = i + 1
+feedF = feedparser.parse( url )
+feedList=[]
+placesInFeed=[]
+i=0
+while (i<len(feedF['entries'])):
+    # print(feedF['entries'][i]['title'])
+    feedList.append(feedF['entries'][i]['title'])
+    print("feed"+str(i)+" : "+feedList[i])
+    i=i+1
 
-    #
-    places = geograpy.get_place_context(text=feedList[2])
+#
+places = geograpy.get_place_context(text=feedList[2])
+placesInFeed.append(places.places)
+print("places - %s" % [str(x) for x in placesInFeed])
+while (i<len(feedList)):
+    places = geograpy.get_place_context(text=feedList[i])
     placesInFeed.append(places.places)
-    print("places - %s" % [str(x) for x in placesInFeed])
-    while (i < len(feedList)):
-        places = geograpy.get_place_context(text=feedList[i])
-        placesInFeed.append(places.places)
-
-    listItem = listItem + 1
+    #print("places - "+str(placesInFeed[i]))
 
 
-
-
-
-    # print("places - "+str(placesInFeed[i]))
 
 # while (i<len(placesInFeed)):
 #     print("places - %s" % [str(x) for x in placesInFeed[i]])
---
+++ News.py
@@ -0,0 +1,4 @@
+class News:
+
+    locations=[]
+    description=""
