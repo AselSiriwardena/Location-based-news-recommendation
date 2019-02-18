26c227877e3445f6b86ddf5203b0e3f2067d7c23
AselIndula <aselindula@gmail.com>
2019-02-18 18:33:45 +0530

    Reading multiple rss feeds

--- Backend/rss_scraper.py
+++ Backend/rss_scraper.py
@@ -2,31 +2,31 @@ import feedparser
 import json
 import geograpy
 
-url = "http://www.adaderana.lk/rss.php"
-# url = "http://www.hirunews.lk/rss/english.xml"
-# url = "http://fe1.virakesari.lk/feed" #getting another language RSS feed
+urlList = ["http://www.adaderana.lk/rss.php", "http://www.hirunews.lk/rss/english.xml", "http://fe1.virakesari.lk/feed"]
+listItem = 0
 
-
-feedF = feedparser.parse( url )
-feedList=[]
-placesInFeed=[]
-i=0
-while (i<len(feedF['entries'])):
-    # print(feedF['entries'][i]['title'])
-    feedList.append(feedF['entries'][i]['title'])
-    print("feed"+str(i)+" : "+feedList[i])
-    i=i+1
+while (listItem < len(urlList)):
+    feedF = feedparser.parse(urlList[listItem])
+    feedList = []
+    placesInFeed = []
+    i = 0
+    while (i < len(feedF['entries'])):
+        feedList.append(feedF['entries'][i]['title'])
+        print("feed " + str(i) + " : " + feedList[i])
+        i = i + 1
 
 #
-places = geograpy.get_place_context(text=feedList[2])
-placesInFeed.append(places.places)
-print("places - %s" % [str(x) for x in placesInFeed])
-while (i<len(feedList)):
-    places = geograpy.get_place_context(text=feedList[i])
+    places = geograpy.get_place_context(text=feedList[2])
     placesInFeed.append(places.places)
-    #print("places - "+str(placesInFeed[i]))
+    print("places - %s" % [str(x) for x in placesInFeed])
+    while (i < len(feedList)):
+        places = geograpy.get_place_context(text=feedList[i])
+        placesInFeed.append(places.places)
+
+    listItem = listItem + 1
 
 
+    # print("places - "+str(placesInFeed[i]))
 
 # while (i<len(placesInFeed)):
 #     print("places - %s" % [str(x) for x in placesInFeed[i]])
