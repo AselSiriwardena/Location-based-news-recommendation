5712be4dcef4394ae5097849c4f922557f1b0083
PeshalaLiyanage <peshala1996@gmail.com>
2019-02-19 00:52:10 +0530

    Alternative solution for reading multiple rss feeds

--- Backend/rss_scraper.py
+++ Backend/rss_scraper.py
@@ -3,11 +3,16 @@ import json
 import geograpy
 
 url = "http://www.adaderana.lk/rss.php"
+url2 = "http://www.hirunews.lk/rss/english.xml"
+url3 = "http://www.hirunews.lk/rss/english.xml"
 # url = "http://www.hirunews.lk/rss/english.xml"
 # url = "http://fe1.virakesari.lk/feed" #getting another language RSS feed
 
 
-feedF = feedparser.parse( url )
+feedF = feedparser.parse(url2)
+feedF2 = feedparser.parse(url)
+feedF3 = feedparser.parse(url3)
+
 feedList=[]
 feedContent=[]
 placesInFeed=[[]]
@@ -18,6 +23,23 @@ while (i<len(feedF['entries'])):
     feedContent.append(feedF['entries'][i]['summary'])
     print("feed"+str(i)+" : "+feedList[i])
     i=i+1
+
+j=0
+while (j<len(feedF2['entries'])):
+    # print(feedF['entries'][i]['title'])
+    feedList.append(feedF2['entries'][j]['title'])
+    feedContent.append(feedF2['entries'][j]['summary'])
+    print("feed"+str(i+j)+" : "+feedList[i+j])
+    j=j+1
+
+
+k=0
+while (k<len(feedF3['entries'])):
+    # print(feedF['entries'][i]['title'])
+    feedList.append(feedF3['entries'][k]['title'])
+    feedContent.append(feedF3['entries'][k]['summary'])
+    print("feed"+str(i+j+k)+" : "+feedList[i+j+k])
+    k=k+1
 
 #
 # places = geograpy.get_place_context(text=feedList[1])
