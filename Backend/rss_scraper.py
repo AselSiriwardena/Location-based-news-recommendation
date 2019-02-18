f5985e3a99893564ef6a34080883b3751365d9b7
PeshalaLiyanage <peshala1996@gmail.com>
2019-02-18 21:28:22 +0530

    replacing places.py

--- Backend/rss_scraper.py
+++ Backend/rss_scraper.py
@@ -9,7 +9,7 @@ url = "http://www.adaderana.lk/rss.php"
 
 feedF = feedparser.parse( url )
 feedList=[]
-placesInFeed=[]
+placesInFeed=[[]]
 i=0
 while (i<len(feedF['entries'])):
     # print(feedF['entries'][i]['title'])
@@ -18,15 +18,16 @@ while (i<len(feedF['entries'])):
     i=i+1
 
 #
-places = geograpy.get_place_context(text=feedList[1])
-placesInFeed.append(places.places)
-print("places - %s" % [str(x) for x in placesInFeed])
-while (i<len(feedList)):
-    places = geograpy.get_place_context(text=feedList[i])
+# places = geograpy.get_place_context(text=feedList[1])
+# placesInFeed.append(places.places)
+# print("places - %s" % [str(x) for x in placesInFeed])
+j=0
+while (j<len(feedList)):
+    places = geograpy.get_place_context(text=feedList[j])
     placesInFeed.append(places.places)
-    #print("places - "+str(placesInFeed[i]))
+    j=j+1
 
 
-
+print("places - %s" % [str(x) for x in placesInFeed])
 # while (i<len(placesInFeed)):
 #     print("places - %s" % [str(x) for x in placesInFeed[i]])
