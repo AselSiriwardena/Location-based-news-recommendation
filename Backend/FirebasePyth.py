fa9348d0de1fcd0b06e08479d7f79d7597d6b178
Rishier827 <nishara.2017431@iit.ac.lk>
2019-02-19 19:18:29 +0530

    change to private realtimedatabase

--- Backend/FirebasePyth.py
+++ Backend/FirebasePyth.py
@@ -1,13 +1,23 @@
 # from firebase import firebase
 # print('Hello Nish')
 import pyrebase
+# public database
+# config = {
+#     "apiKey": "AIzaSyDsTRtMepuPzzlHIlwP5t-vYqelNJFQLf0",
+#     "authDomain": "locationpython-3edc5.firebaseapp.com",
+#     "databaseURL": "https://locationpython-3edc5.firebaseio.com",
+#     "projectId": "locationpython-3edc5",
+#     "storageBucket": "locationpython-3edc5.appspot.com",
+#     "messagingSenderId": "335224995856"
+#   }
+#private database
 config = {
-    "apiKey": "AIzaSyDsTRtMepuPzzlHIlwP5t-vYqelNJFQLf0",
-    "authDomain": "locationpython-3edc5.firebaseapp.com",
-    "databaseURL": "https://locationpython-3edc5.firebaseio.com",
-    "projectId": "locationpython-3edc5",
-    "storageBucket": "locationpython-3edc5.appspot.com",
-    "messagingSenderId": "335224995856"
+    "apiKey": "AIzaSyD8oXsTwcGZK738DlKbdU2wfsyo-pKPD50",
+    "authDomain": "myfirst-62243.firebaseapp.com",
+    "databaseURL": "https://myfirst-62243.firebaseio.com",
+    "projectId": "myfirst-62243",
+    "storageBucket": "myfirst-62243.appspot.com",
+    "messagingSenderId": "505364342015"
   }
 
 firebase = pyrebase.initialize_app(config)
