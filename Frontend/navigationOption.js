18208da73a5f957d43e30e38e4c9e16806b6554a
Kalana Bandara <44183583+Kalana97@users.noreply.github.com>
2019-02-21 21:57:01 +0530

    Adding navigation Option

---
+++ Frontend/navigationOption.js
@@ -0,0 +1,12 @@
+navigationOptions: {
+  header: props => <CustomHeader {...props} />,
+  headerStyle: {
+    backgroundColor: "transparent"
+  },
+  headerTitleStyle: {
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  headerTintColor: "#fff"
+  animationEnabled: true
+}
