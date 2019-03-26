1121d7546a611c781a54a2f8d7da6f24dd796a9f
PeshalaLiyanage <peshala1996@gmail.com>
2019-03-27 00:16:32 +0530

    After the merge

--- Backend/rss_scraper.py
+++ Backend/rss_scraper.py
@@ -1,7 +1,9 @@
 import feedparser
 import json
 import geograpy
+import nltk
 
+nltk.download('stopwords')
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
@@ -15,7 +17,7 @@ url = ["http://www.adaderana.lk/rss.php",
        "https://www.news.lk/news?format=feed",
        "https://srilankamirror.com/news?format=feed&type=rss",
        "http://www.thesundayleader.lk/feed/",
-       "https://www.newsfirst.lk/feed/",
+       "https://www.newsfirst.lk/feed/"
        ]
 
 feedTitle = []
@@ -58,6 +60,9 @@ print("#########################################################################
 
 
 #####
+businessData=[]
+politicalData=[]
+entertainmentData=[]
 
 def stop_words_remove(sentence):
     stop_words = set(stopwords.words('english'))
@@ -68,32 +73,27 @@ def stop_words_remove(sentence):
             return w
 
 
-businessData = []
-politicalData = []
 
-f = open(
-    "/home/peshala/PycharmProjects/Test/Datasets/news-articles-dataset-master/news/business/world-leaders-gather-to-face-uncertainty.txt",
-    "r")
-for x in f:
+
+businessDataFile = open("/home/peshala/PycharmProjects/Test/Datasets/news-articles-dataset-master/news/businessdata.txt", "r")
+for x in businessDataFile:
     businessData.append(stop_words_remove(x))
 
-f2 = open(
-    "/home/peshala/PycharmProjects/Test/Datasets/news-articles-dataset-master/news/business/wal-mart-fights-back-at-accusers.txt",
-    "r")
-for x in f2:
-  businessData.append(stop_words_remove(x))
-
-f3 = open(
-    "/home/peshala/PycharmProjects/Test/Datasets/news-articles-dataset-master/news/politics/lib-dems-target-the-student-vote.txt",
-    "r")
-for x in f3:
-    politicalData.append(stop_words_remove(x))
-
-f4 = open(
-    "/home/peshala/PycharmProjects/Test/Datasets/news-articles-dataset-master/news/politics/minimum-rate-for-foster-parents.txt",
-    "r")
-for x in f4:
-    politicalData.append(stop_words_remove(x))
+# f2 = open(
+#     "/home/peshala/PycharmProjects/Test/Datasets/news-articles-dataset-master/news/business/wal-mart-fights-back-at-accusers.txt",
+#     "r")
+# for x in f2:
+#   businessData.append(stop_words_remove(stop_words_remove(x)))
+#
+# entertainmentDataFile = open("/home/peshala/PycharmProjects/Test/Datasets/news-articles-dataset-master/news/politics/lib-dems-target-the-student-vote.txt", "r")
+# for x in entertainmentDataFile:
+#     entertainmentData.append(stop_words_remove(x))
+#
+# f4 = open(
+#     "/home/peshala/PycharmProjects/Test/Datasets/news-articles-dataset-master/news/politics/minimum-rate-for-foster-parents.txt",
+#     "r")
+# for x in f4:
+#     politicalData.append(stop_words_remove(x))
 # for x in businessData:
 #     print(x)
 ######
@@ -103,6 +103,44 @@ repeat = 0
 
 class News(object):
     pass
+
+
+def categriser(l):
+
+    bCount=0
+    pCount=0
+    eCount=0
+    sCount=0
+    tCount=0
+    bAverage=0
+    pAverage=0
+    eAverage=0
+
+    categoryPercentage=[]
+    categoryName=[]
+
+    for x in businessData:
+        bCount = bCount + 1
+        bAverage = bAverage + fuzz.partial_ratio(x, feedContent[l - 1])
+    for x in politicalData:
+        pCount = pCount + 1
+        pAverage = pAverage + fuzz.partial_ratio(x, feedContent[l - 1])
+    for x in entertainmentData:
+        eCount = eCount + 1
+        eAverage = eAverage + fuzz.partial_ratio(x, feedContent[l - 1])
+
+    businessPercentage=bAverage/bCount
+    politicalPercentage=pAverage/pCount
+    entertainmentPercentage=eAverage/eCount
+
+    categoryPercentage.append(businessPercentage)
+    categoryPercentage.append(entertainmentPercentage)
+    categoryPercentage.append(politicalPercentage)
+
+
+    maxValue=categoryPercentage[0]
+    print(maxValue)
+    for
 
 while True:
 
@@ -116,41 +154,43 @@ while True:
                 # print("test :" + str(placesInFeed[l][i]))
                 print(" News : " + feedTitle[l - 1])
                 print(" Content : " + feedContent[l - 1])
+                categriser(l)
+
                 # print("index:"+str(l))
                 ###
-                similarityB = 0
-                avCountB = 0
-                for x in businessData:
-                    avCountB = avCountB + 1
-                    similarityB = similarityB + fuzz.partial_ratio(x, feedContent[l - 1])
+                # similarityB = 0
+                # avCountB = 0
+                # for x in businessData:
+                #     avCountB = avCountB + 1
+                #     similarityB = similarityB + fuzz.partial_ratio(x, feedContent[l - 1])
 
-                print('avcountB :' + str(avCountB) + ' similarityB :' + str(similarityB))
-                averageB = similarityB / avCountB
-
-                print('average: ' + str(averageB))
-                ###
-
-                similarityE = 0
-                avCountE = 0
-                for x in politicalData:
-                    avCountE = avCountE + 1
-                    similarityE = similarityE + fuzz.partial_ratio(x, feedContent[l - 1])
-
-                print('avcountE :' + str(avCountE) + 'similarityE:' + str(similarityE))
-                averageE = similarityE / avCountE
+                # print('avcountB :' + str(avCountB) + ' similarityB :' + str(similarityB))
+                # averageB = similarityB / avCountB
+                #
+                # print('average: ' + str(averageB))
+                # ###
+                #
+                # similarityE = 0
+                # avCountE = 0
+                # for x in politicalData:
+                #     avCountE = avCountE + 1
+                #     similarityE = similarityE + fuzz.partial_ratio(x, feedContent[l - 1])
+                #
+                # print('avcountE :' + str(avCountE) + 'similarityE:' + str(similarityE))
+                # averageE = similarityE / avCountE
 
                 # print(x)
                 #
-                print('averageE: ' + str(averageE))
+                # print('averageE: ' + str(averageE))
                 ####
 
-                if (averageB > averageE):
-                    News.category = 'Bussiness'
-                    print('Category :' + News.category)
-
-                else:
-                    News.category = 'Political'
-                    print('Category :' + News.category)
+                # if (averageB > averageE):
+                #     News.category = 'Bussiness'
+                #     print('Category :' + News.category)
+                #
+                # else:
+                #     News.category = 'Political'
+                #     print('Category :' + News.category)
 
             entityCount = entityCount + 1
         l = l + 1
@@ -159,3 +199,6 @@ while True:
 
 # while (i<len(placesInFeed)):
 #     print("places - %s" % [str(x) for x in placesInFeed[i]])
+
+
+
