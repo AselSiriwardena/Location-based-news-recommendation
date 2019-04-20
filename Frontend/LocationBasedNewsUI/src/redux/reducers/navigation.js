745eea668b25447f5fc80f88d2ad21615d3c03ec
Rishie Ramasinghe <nishara.2017431@iit.ac.lk>
2019-04-20 23:54:04 +0530

    Adding star rating and fav

--- Frontend/LocationBasedNewsUI/src/redux/reducers/navigation.js
+++ Frontend/LocationBasedNewsUI/src/redux/reducers/navigation.js
@@ -2,7 +2,7 @@ import createReducer from '../helpers/createReducer';
 import { NavigationActions } from 'react-navigation';
 import { AppNavigator } from '../../navigators/AppNavigator';
 
-const firstAction = AppNavigator.router.getActionForPathAndParams('LoggedOut');
+const firstAction = AppNavigator.router.getActionForPathAndParams('LoggedIn');
 const initialNavState = AppNavigator.router.getStateForAction(firstAction);
 
 export const nav = (state = initialNavState, action) => {
--- Frontend/LocationBasedNewsUI/src/styles/colors/index.js
+++ Frontend/LocationBasedNewsUI/src/styles/colors/index.js
@@ -8,4 +8,10 @@ export default {
   lightGray: '#d8d8d8',
   pink: '#fc4c54',
     gray01: '#f3f3f3',
+  gray02: '#919191',
+  gray03: '#b3b3b3',
+  gray04: '#484848',
+  brown01: '#ad8763',
+  brown02: '#7d4918',
+  blue: '#4995cd',
 }
