9c95eb813a8b824e21571159f72aeed397cc71ab
Rishie Ramasinghe <nishara.2017431@iit.ac.lk>
2019-04-30 12:45:04 +0530

    Fixing upto AdminLeve1 to 2 Location issue

--- Frontend/FixingLocationIssues/Location/App.js
+++ Frontend/FixingLocationIssues/Location/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import { StyleSheet,
-  Text,
-  View,
-TouchableOpacity } from 'react-native';
-import Geocoder from 'react-native-geocoding';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View,Dimensions,AppRegistry,TouchableOpacity} from 'react-native';
+// import {Header,Right,Left,Icon,Container} from 'native-base'
 import RNAndroidLocationEnabler from 'react-native-android-location-enabler';
+import Geocoder from 'react-native-geocoding';
+
+
+
 RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({interval: 10000, fastInterval: 5000})
   .then(data => {
     // The user has accepted to enable the location services
@@ -19,94 +20,87 @@ RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({interval: 10000, fastI
     //  - ERR01 : If the Settings change are unavailable
     //  - ERR02 : If the popup has failed to open
   });
+  export default class HomeScreen extends Component {
 
-class Home extends Component {
 	constructor(props) {
-		super(props);
+	  super(props);
+  
+	  this.state = {
+		latitude: null,
+		longitude: null,
+		error:null,
+	  };
+	}
+   
+	componentDidMount() {
+	  navigator.geolocation.getCurrentPosition(
+		 (position) => {
+		   console.log("wokeeey");
+		   console.log(position);
+		   this.setState({
+			 latitude: position.coords.latitude,
+			 longitude: position.coords.longitude,
+			 error: null,
+		   });
+		 },
+		 (error) => this.setState({ error: error.message }),
+		 { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+	   );
+	 }
+  
+	getData(){
+		
+		Geocoder.init("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+  
+	  Geocoder.from(this.state.latitude,this.state.longitude)
+		  .then(json => {
+			  var addressComponent = json.results[0].address_components[0];
+       var stateName = json.results[0].address_components.filter(x => x.types.filter(t => t == 'administrative_area_level_2').length > 0)[0].short_name;
+      //  var stateName = json.results[0].address_components.filter(x => x.types.filter(t => t == 'colloquial_area').length > 0)[0].short_name;
+		   // alert(JSON.stringify(json.results[0].address_components));
+	  alert(stateName);
+		  })
+		  .catch(error => console.warn(error));
+  
+	}
 	
-		this.state = {
-			latitude : 28.6139,
-	longitude : 77.2090,
-		  error: null,
-		};
-	  }
-	//   componentDidMount() {
-	// 	navigator.geolocation.getCurrentPosition(
-	// 	  (position) => {
-	// 		this.setState({
-	// 		  latitude: position.coords.latitude,
-	// 		  longitude: position.coords.longitude,
-	// 		  error: null,
-	// 		});
-	// 	  },
-	// 	  (error) => this.setState({ error: error.message }),
-	// 	  { enableHighAccuracy: true, timeout: 10000, maximumAge: 1000 },
-	// 	);
-	//   }
-
-  getData(){
-    // Geocoder.setApiKey("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-	Geocoder.init("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-	// Geocoder.init('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-
-    Geocoder.from(this.state.latitude, this.state.longitude)
-		.then(json => {
-//         		var addressComponent = json.results[0].formatted_address;
-//       console.log(addressComponent);
-//       console.log(addressComponent)
-// 	  alert(addressComponent);
-// })
-// .catch(error => console.warn(error));
-	var addressComponent = json.results[0].address_components[0];
-           var stateName = json.results[0].address_components.filter(x => x.types.filter(t => t == 'administrative_area_level_2').length > 0)[0].short_name;
-         // alert(JSON.stringify(json.results[0].address_components));
-    alert(stateName);
-		})
-  	.catch(error => console.warn(error));
-  // Geocoder.from("Colosseum")
-	// 	.then(json => {
-	// 		var location = json.results[0].geometry.location;
-  //     // console.log(location);
-  //     alert(location);
-	// 	})
-	// 	.catch(error => console.warn(error));
+	// static navigationOptions={
+	//   drawerIcon:({tintColor})=>(
+	// 	<Icon name="md-wifi" style={{fontSize:24,color:tintColor}}/>
+	//   )
+	// }
+	render() {
+	 
+	  return (
+		 <View style={styles.container}>
+  
+		
+  
+		   <View>
+			 
+		  <Text> Longitude:- {this.state.latitude} </Text>
+		  <Text> Latitude:- {this.state.longitude} </Text>
+		  <Text> {this.state.error} </Text>
+		  
+		</View>
+		<View>
+		  <TouchableOpacity onPress={()=>{this.getData()}}>
+		  <Text style={{fontSize:20}} >Get Address</Text>
+		</TouchableOpacity>
+		</View>
+		</View>
+	  );
+	}
   }
   
-  render() {
-    return (
-      <View style={styles.container}>
-      <TouchableOpacity onPress={()=>{this.getData()}}>
-        <Text>Get Address</Text>
-        </TouchableOpacity>
-      </View>
-    );
+  const styles = StyleSheet.create({
+	container: {
+	  flex: 1,
+	
+	},
+	
+	
+	big: {
+	  fontSize: 20
   }
-}
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#F5FCFF',
-    flex: 10,
-    justifyContent: 'center'
-  },
-});
-
-export default Home
-
-
-// Works as well :
-// ------------
-
-// location object
-// Geocoder.from({
-// 	latitude : 41.89,
-// 	longitude : 12.49
-// });
-
-// latlng object
-// Geocoder.from({
-// 	lat : 41.89,
-// 	lng : 12.49
-// });
-
-// array
-// Geocoder.from([41.89, 12.49]);
+  });
