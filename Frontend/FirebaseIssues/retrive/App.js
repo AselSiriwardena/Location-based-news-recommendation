78e3948a1103a7d6de3579c17645e1c1b117412b
Kalana bandara <rmkalana97@gmail.com>
2019-05-06 12:57:30 +0530

    modified fetching and retring part

--- Frontend/FirebaseIssues/retrive/App.js
+++ Frontend/FirebaseIssues/retrive/App.js
@@ -9,51 +9,63 @@ export default class FetchExample extends React.Component {
   }
 
   componentDidMount(){
-    fetch('http://192.168.8.101:8000/category/', {
-      method: 'POST',
+  //   fetch('http://192.168.8.101:8000/category/', {
+  //     method: 'POST',
       
-      headerss: {
-          'Accept': 'application/json',
-          "Content-Type": "application/json",                
-      },
-      body: JSON.stringify({
-          cname: 'fuck',
+  //     headerss: {
+  //         'Accept': 'application/json',
+  //         "Content-Type": "application/json",                
+  //     },
+  //     body: JSON.stringify({
+  //         cname: 'fuck',
          
+  //     })
+  // })
+  //     .then((resp)=>{
+  //         return resp.json();
+  //     })
+  //     .then((jsonData) => {
+  //         console.log(JSON.stringify(jsonData));
+  //         // if(jsonData['result'] == true){
+  //         //     AsyncStorage.setItem('USER', jsonData.user);
+  //         //     AsyncStorage.setItem('TOKEN', jsonData.token);
+  //         //     alert("You are: "+jsonData['user']);
+  //         //     this.props.navigation.navigate('Dashboard');
+  //         // }
+  //         // else{
+  //         //   alert("Wrong uername/password. Try again");
+  //         // }
+  //     })
+  //     .catch((e)=>{
+  //         console.log(e);
+  //     })
+    // return fetch('http://192.168.8.101:8000/category/')
+   return fetch('http://192.168.8.101:8000/category/', {
+          method: 'POST',
+          
+          headerss: {
+              'Accept': 'application/json',
+              "Content-Type": "application/json",                
+          },
+          body: JSON.stringify({
+              cname: 'sports',
+             
+          })
       })
-  })
-      .then((resp)=>{
-          return resp.json();
-      })
-      .then((jsonData) => {
-          console.log(JSON.stringify(jsonData));
-          // if(jsonData['result'] == true){
-          //     AsyncStorage.setItem('USER', jsonData.user);
-          //     AsyncStorage.setItem('TOKEN', jsonData.token);
-          //     alert("You are: "+jsonData['user']);
-          //     this.props.navigation.navigate('Dashboard');
-          // }
-          // else{
-          //   alert("Wrong uername/password. Try again");
-          // }
-      })
-      .catch((e)=>{
-          console.log(e);
-      })
-    // return fetch('http://192.168.8.100:8000/category/')
-    //   .then((response) => response.json())
-    //   .then((responseJson) => {
+      .then((response) => response.json())
+      .then((responseJson) => {
 
-    //     this.setState({
-    //       isLoading: false,
-    //       dataSource: responseJson,
-    //     }, function(){
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson,
+        }, function(){
 
-    //     });
+        });
 
-    //   })
-    //   .catch((error) =>{
-    //     console.error(error);
-    //   });
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
 //     fetch('http://192.168.8.100:8000/category/', {
 //   method: 'POST',
 //   headers: {
