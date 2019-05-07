0d57485d663a42b9a3cba904cb71ec43aab2b7fb
Rasara Thrilanka <rasarathrilanka@gmail.com>
2019-05-07 10:40:31 +0530

    Profile container added
    without styling

--- Frontend/LocationBasedUI/src/containers/ProfileContainer.js
+++ Frontend/LocationBasedUI/src/containers/ProfileContainer.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import {
-  View,
-  Text,
   StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -19,17 +21,33 @@ export default class InboxContainer extends Component {
   };
 
   render() {
+    // return (
+    //   <View style={styles.wrapper}>
+    //     <Text>Profile Container</Text>
+    //   </View>
+    // );
     return (
-      <View style={styles.wrapper}>
-        <Text>Profile Container</Text>
+      <View style={styles.container}>
+          <View style={styles.header}></View>
+          <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>John Doe</Text>
+              {/* <Text style={styles.info}>UX Designer / Mobile developer</Text> */}
+              {/* <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text> */}
+              
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Edit User Preferences</Text>  
+              </TouchableOpacity>              
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Edit Profile</Text> 
+                </TouchableOpacity>
+                   
+            </View>
+        </View>
       </View>
     );
   }
 };
 
-const styles = StyleSheet.create({
-  wrapper: {
-    display: 'flex',
-    padding: 50,
-  }
 });
