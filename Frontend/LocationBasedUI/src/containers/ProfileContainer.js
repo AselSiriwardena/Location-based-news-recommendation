f11395bcb7676de6ab8c0e6d6811cc465d128fae
Rasara Thrilanka <rasarathrilanka@gmail.com>
2019-05-07 10:42:46 +0530

    Profile container styling edited.

--- Frontend/LocationBasedUI/src/containers/ProfileContainer.js
+++ Frontend/LocationBasedUI/src/containers/ProfileContainer.js
@@ -50,4 +50,66 @@ export default class InboxContainer extends Component {
   }
 };
 
+
+const styles = StyleSheet.create({
+  wrapper: {
+    display: 'flex',
+    padding: 50,
+  },
+  header:{
+    backgroundColor: "#00BFFF",
+    height:100,
+  },
+  avatar: {
+    width: 110,
+    height: 110,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom:10,
+    alignSelf:'center',
+    position: 'absolute',
+    marginTop:60
+  },
+  name:{
+    fontSize:22,
+    color:"#FFFFFF",
+    fontWeight:'600',
+  },
+  body:{
+    marginTop:40,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: 'center',
+    padding:30,
+  },
+  name:{
+    fontSize:28,
+    color: "#696969",
+    fontWeight: "600"
+  },
+  info:{
+    fontSize:16,
+    color: "#00BFFF",
+    marginTop:10
+  },
+  description:{
+    fontSize:16,
+    color: "#696969",
+    marginTop:10,
+    textAlign: 'center'
+  },
+  buttonContainer: {
+    marginTop:10,
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+    backgroundColor: "#00BFFF",
+  },
+
 });
